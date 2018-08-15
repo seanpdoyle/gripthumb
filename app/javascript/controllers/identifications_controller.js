efadba2501afeb5f4783368afe0f67a39a27edc9
Sean Doyle <sean.p.doyle24@gmail.com>
2018-08-15 16:26:33 -0400

    Simplify `identifications` controller
    Style the `[data-target="identifications.recordButton"]` element to make
    it easier to tap on smaller devices.
    
    Once recording, set `<button disabled>` and add an appropriate class to
    the controller element. Once results have been published, removed the
    class and enable the `<button>`.
    
    Remove the `<ul>` from the page, since we'll be redirecting once we get
    results.

--- app/javascript/controllers/identifications_controller.js
+++ app/javascript/controllers/identifications_controller.js
@@ -1,22 +1,29 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  static targets = ["form", "recordButton", "results"]
+  static targets = ["form", "artist", "title", "recordButton"]
 
   connect() {
     if (window.webkit) {
-      this.recordButtonTarget.classList.add("record-button--enabled");
+      this.recordButtonTarget.classList.add("record-button--enabled")
       this.formTarget.classList.remove("form--enabled")
     }
   }
 
   record() {
+    this.recordButtonTarget.disabled = true
+    this.element.classList.add("identifications--recording")
     this.gripthumb.postMessage("startRecording")
   }
 
   publish(event) {
-    let tracks = event.detail
-    this.resultsTarget.innerHTML = JSON.stringify(tracks);
+    let [track] = event.detail
+    this.element.classList.remove("identifications--recording")
+    this.recordButtonTarget.disabled = false
+
+    this.artistTarget.value = track.artist
+    this.titleTarget.value = track.title
+    this.formTarget.submit()
   }
 
   get gripthumb() {
