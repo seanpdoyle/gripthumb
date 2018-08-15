a476707de1b18bc6b9cc7288426dd07fe0b55868
Sean Doyle <sean.p.doyle24@gmail.com>
2018-08-15 12:58:10 -0400

    Build Stylesheets
    Import `rails-ujs` and start in `packs/application.js`.
    
    Configure `styles/application.scss` to be included in the HTML.
    
    Import `bourbon/core/bourbon` in the styles.
    Import `neat/core/neat` in the styles.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,9 +1,12 @@
+import "style/application.scss"
 import { Application } from "stimulus"
 import { definitionsFromContext } from "stimulus/webpack-helpers"
+import Rails from "rails-ujs"
 import Turbolinks from "turbolinks"
 
 const application = Application.start()
 const context = require.context("controllers", true, /.js$/)
 application.load(definitionsFromContext(context))
 
+Rails.start();
 Turbolinks.start();
