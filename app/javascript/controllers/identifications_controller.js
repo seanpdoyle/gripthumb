58c15238056628a02c9d14f43f60ea306b5e41ba
Sean Doyle <sean.p.doyle24@gmail.com>
2018-08-18 13:56:59 -0400

    Respond to `gripthumb:progress` event
    Translate `gripthumb:progress` events published on the `document` into
    `progress[value]` attribute.

--- app/javascript/controllers/identifications_controller.js
+++ app/javascript/controllers/identifications_controller.js
@@ -1,13 +1,23 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  static targets = ["form", "artist", "song", "recordButton"]
+  static targets = ["form", "artist", "song", "recordButton", "progress"]
+
+  initialize() {
+    this.progress = 0
+  }
 
   connect() {
     if (window.webkit) {
       this.recordButtonTarget.classList.add("songs--record-button__enabled")
       this.formTarget.classList.remove("songs--form__enabled")
     }
+  }
+
+  trackProgress(event) {
+    let progress = event.detail.progress;
+
+    this.progress = progress;
   }
 
   record() {
@@ -28,5 +38,14 @@ export default class extends Controller {
 
   get gripthumb() {
     return window.webkit.messageHandlers.GripThumb
+  }
+
+  set progress(percent) {
+    this.progressTarget.value = percent
+    this.progressTarget.text = `${percent}%`
+  }
+
+  get progress() {
+    return parseInt(this.progressTarget.value, 10);
   }
 }
