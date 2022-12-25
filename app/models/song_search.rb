0323ce0ccb49c3a4ae53a4830376b612679fc5eb
Sean Doyle <sean.p.doyle24@gmail.com>
2022-12-25 10:42:06 -0500

    Inline `SongSearch::Crawler` into `SongSearch`
    Since the SkateVideoSite results are parsed from JSON instead of HTML,
    the separate layer of indirection introduced by the
    `SongSearch::Crawler` isn't worthwhile.
    
    This commit inlines the code back into the `SongSearch` class that
    originally called it.

--- app/models/song_search.rb
+++ app/models/song_search.rb
@@ -1,8 +1,30 @@
 class SongSearch < ApplicationModel
+  Result = Data.define(:video, :part)
+
   attribute :cache, default: {}
   attribute :song
 
   def results
-    SongSearch::Crawler.new(song).results
+    cache.fetch(search_url) do
+      response = JSON.parse(URI.parse(search_url).read, symbolize_names: true)
+
+      response.fetch(:song_results, []).flat_map do |search_result|
+        video = search_result.fetch(:title)
+
+        search_result.fetch(:soundtracks, []).map do |soundtrack|
+          Result.new(video, soundtrack.fetch(:section))
+        end
+      end
+    end
+  end
+
+  private
+
+  def search_text
+    [song.artist, song.name].join(" ").squish.downcase.gsub(/[^[:word:]\s]/i, "")
+  end
+
+  def search_url
+    "https://skatevideosite.com/api/search?" + {search_text:}.to_query
   end
 end
